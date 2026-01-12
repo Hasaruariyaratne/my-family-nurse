daeb4181eaddad10f3520e30dc394e3b743119cf
Hasaruariyaratne <ushanhasaranga2003@gmail.com>
2026-01-12 15:35:03 +0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/my-family-nurse/',
   plugins: [react()],
   server: {
-    host: true, // Enable Network Access
-    port: 3000, // Change this value to change the port number
+    host: true,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
